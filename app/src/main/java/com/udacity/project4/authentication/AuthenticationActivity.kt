0bb7b7f3c0ffb4cb511111ddcc8cadd963bf752f
Ryan Wong <ryan@rwmobi.com>
2021-09-19 05:27:41 +0100

    [feature] Login: link login button to Firebase Login; add redirection to ReminderList if logged in

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -5,11 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.R
 import com.udacity.project4.authentication.AuthenticationViewModel.AuthenticationState.AUTHENTICATED
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -21,14 +24,18 @@ class AuthenticationActivity : AppCompatActivity() {
         const val SIGN_IN_RESULT_CODE = 1001
     }
 
+    lateinit var binding: ActivityAuthenticationBinding
     private val authenticationViewModel by viewModels<AuthenticationViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+
         // Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
         // If the user was authenticated, send him to RemindersActivity
         observeAuthenticationState()
+
+        binding.buttonLogin.setOnClickListener { launchFirebaseLogin() }
     }
 
 
@@ -72,13 +79,13 @@ class AuthenticationActivity : AppCompatActivity() {
      */
     private fun observeAuthenticationState() {
         authenticationViewModel.authenticationState.observe(this, Observer { authenticationState ->
-            // TODO 1. Use the authenticationState variable you just added
-            // in LoginViewModel and change the UI accordingly.
             if (authenticationState == AUTHENTICATED) {
                 // Upon successful login, navigate the user to the Reminders screen
                 // Remainder screen terminated when navigating to this screen, 
                 // therefore we cannot use back.
-                
+                val intent = Intent(this, RemindersActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
             }
             // If not logged in, stay on this screen waiting for the user to click on the login button
         })
