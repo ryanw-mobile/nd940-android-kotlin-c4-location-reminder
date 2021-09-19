d15023f9a1aaf554c8c66c0e25a3be3b9ba6829a
Ryan Wong <ryan@rwmobi.com>
2021-09-19 05:41:57 +0100

    [refactor] AuthenticationActivity: remove StartActivityForResult() usage as it is redundant

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,6 +1,5 @@
 package com.udacity.project4.authentication
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -8,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.R
 import com.udacity.project4.authentication.AuthenticationViewModel.AuthenticationState.AUTHENTICATED
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
@@ -19,10 +17,6 @@ import com.udacity.project4.locationreminders.RemindersActivity
  * signed in users to the RemindersActivity.
  */
 class AuthenticationActivity : AppCompatActivity() {
-
-    companion object {
-        const val SIGN_IN_RESULT_CODE = 1001
-    }
 
     lateinit var binding: ActivityAuthenticationBinding
     private val authenticationViewModel by viewModels<AuthenticationViewModel>()
@@ -38,37 +32,21 @@ class AuthenticationActivity : AppCompatActivity() {
         binding.buttonLogin.setOnClickListener { launchFirebaseLogin() }
     }
 
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SIGN_IN_RESULT_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                // TODO: User successfully logged in
-
-            } else {
-                // TODO: log in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-            }
-        }
-    }
-
     private fun launchFirebaseLogin() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
 
-        // Create and launch sign-in intent.
-        // We listen to the response of this activity with the SIGN_IN_REQUEST_CODE
-        // This is deprecated, I hope Udacity would update their course content in the near future.
-        startActivityForResult(
+        // Create and launch sign-in intent
+        // Since in this App we use the observer to check the login status,
+        // onActivityResult has no use to me, therefore I just do not bother to use
+        // the deprecated startActivityForResult(...)
+        startActivity(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
-                .build(),
-            AuthenticationActivity.SIGN_IN_RESULT_CODE
+                .build()
         )
     }
 
