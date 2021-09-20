bdb270407699d4836a0b22cb648873be2a8135e2
Ryan Wong <ryan@rwmobi.com>
2021-09-20 21:49:19 +0100

    [test] FakeDataSource: create to act as a test double for LocalDataSource

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,25 +4,32 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
-
+    // A fake data source to act as a double to the real data source
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        reminders?.let { return Result.Success(ArrayList(it)) }
+        return Result.Error("Reminder not found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        // Check the list to find a reminder with matching id
+        val reminder = reminders?.find {
+            it.id == id
+        }
+
+        return if (reminder != null)
+            Result.Success(reminder)
+        else Result.Error("Reminder not found")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
-
 
 }
